c9eb0d2b5e9db63f78f31ed6e0de32b65ff573c2
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-09-26 19:27:43 -0400

    Added tasks menu

--- todo_list.py
+++ todo_list.py
@@ -10,4 +10,20 @@ def remove_task(index):
     if 0 <= index < len(tasks):
         del tasks[index]
 
-if __name__ = __main__:
+if __name__ == "__main__":
+    tasks = []
+
+    while True:
+        print("---Todo List---\n")
+        print("  Options")
+        print("  -------  ")
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Update Task")
+        print("4. Remove Task")
+        print("5. Quit")
+        print("----------------")
+
+        userChoice = input("Select which choice you would like: ")
+
+        
