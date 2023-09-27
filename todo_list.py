cd104542249b805748d3a5ad9f3c1911c83ffdbe
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-09-27 15:22:39 -0400

    Added saving and loading to and from a txt file

--- todo_list.py
+++ todo_list.py
@@ -10,8 +10,23 @@ def delete_task(index):
     if 0 <= index < len(tasks):
         del tasks[index]
 
+def save_task_to_file(file_name):
+    with open(file_name, "w") as file:
+        for task in tasks:
+            file.write(f"{task}\n")
+
+def load_task_to_file(file_name):
+    try:
+        with open(file_name, "r") as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+            return []
+
 if __name__ == "__main__":
-    tasks = []
+    file_name = "todolist.txt"
+
+    # file is loaded before program runs
+    tasks = load_task_to_file(file_name)
 
     while True:
         print("\n---Todo List---\n")
@@ -21,7 +36,8 @@ if __name__ == "__main__":
         print("2. View Tasks")
         print("3. Update Task")
         print("4. Delete Task")
-        print("5. Quit")
+        print("5. Save and Exit")
+        print("6. Exit without Saving")
         print("----------------")
 
         userChoice = input("Select which choice you would like: ")
@@ -40,6 +56,9 @@ if __name__ == "__main__":
             index = int(input("Enter the task you wish to remove: ")) -1
             delete_task(index)
         elif userChoice == "5":
+            save_task_to_file(file_name)
+            break
+        elif userChoice == "6":
             break
         else:
             print("That's an invalid option. Please reenter a value.")
