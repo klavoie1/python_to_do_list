f89ec7035062fe501d73841360a8f1412581c93e
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-09-27 15:48:09 -0400

    Added due dates to tasks

--- todo_list.py
+++ todo_list.py
@@ -1,10 +1,10 @@
 
-def add_tasks(task):
-    tasks.append(task)
+def add_tasks(task, date_due):
+    tasks.append((task, date_due))
 
-def update_task(index, updated_task):
+def update_task(index, updated_task, updated_date_due):
     if 0 <= index < len(tasks):
-        tasks[index] = updated_task
+        tasks[index] = (updated_task, updated_date_due)
 
 def delete_task(index):
     if 0 <= index < len(tasks):
@@ -12,15 +12,16 @@ def delete_task(index):
 
 def save_task_to_file(file_name):
     with open(file_name, "w") as file:
-        for task in tasks:
-            file.write(f"{task}\n")
+        for task, date_due in tasks:
+            file.write(f"{task},{date_due}\n")
 
 def load_task_to_file(file_name):
     try:
         with open(file_name, "r") as file:
-            return [line.strip() for line in file.readlines()]
+            lines = file.readlines()
+        return [line.strip().split(",") for line in lines]
     except FileNotFoundError:
-            return []
+        return []
 
 if __name__ == "__main__":
     file_name = "todolist.txt"
@@ -44,14 +45,16 @@ if __name__ == "__main__":
 
         if userChoice == "1":
             task = input("Enter your task: ")
-            add_tasks(task)
+            date_due = input("Enter the due date in the form of mm-dd-yyyy: ")
+            add_tasks(task, date_due)
         elif userChoice == "2":
-            for i, task in enumerate(tasks):
-                print(f"{i + 1}. {task}")   
+            for i, (task, date_due) in enumerate(tasks):
+                print(f"{i + 1}. {task} (Date Due: {date_due})")   
         elif userChoice == "3":
             index = int(input("Enter the number of the task you want to update: ")) -1
             updated_task = input("Enter the updated task: ")
-            update_task(index, updated_task)
+            updated_due_date = input("Enter the new due date in the form of mm-dd-yyyy: ")
+            update_task(index, updated_task, updated_due_date)
         elif userChoice == "4":
             index = int(input("Enter the task you wish to remove: ")) -1
             delete_task(index)
